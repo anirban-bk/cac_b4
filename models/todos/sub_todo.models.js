7eea40153f104cc852f3b0e6821cf9474ce3f2b6
anirban-bk <117968658+anirban-bk@users.noreply.github.com>
2024-05-02 15:45:29 +0000

    sub_todo models committed

--- models/todos/sub_todo.models.js
+++ models/todos/sub_todo.models.js
@@ -1,5 +1,11 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema(
+    {
+        username: {type: String, unique: true, lowercase: true, required: true},
+        password: {type: String, required: [true, "password is required"]},
+        email: {type:String, lowercase: true, required: true, unique: true}
+    }, 
+    {timestamps: true});
 
 export const User = mongoose.model("User", userSchema);
